bee13744d919dd3dd5032423e24f81d4b57b4f48
Kerem Karatal <kkaratal@yahoo.com>
2013-02-10 23:26:06 -0800

    Garantia Redis does not seem to like timeout for redis connection

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -11,5 +11,6 @@ else
   # $redis_analyze will subscribe to the ACTION messages 
   # DO NOT use $redis_analyze for anything other than subscribing, 
   # You will otherwise get an error: ERR only (P)SUBSCRIBE / (P)UNSUBSCRIBE / QUIT allowed in this context
-  $redis_analyze = Redis.new(:timeout => 0, :host => uri.host, :port => uri.port, :password => uri.password)  
+  # Commenting out :timeout=>0. Garantia Redis does not seem to like it
+  $redis_analyze = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
